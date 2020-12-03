9e23f97a24810c5468dc0b418bd5216e0cab4876
amoghkashyap86 <amoghk87kashyap@gmail.com>
2020-12-04 00:07:11 +0530

    db cloud

--- model/databaseconncetivity.js
+++ model/databaseconncetivity.js
@@ -3,10 +3,12 @@ const express = require("express")
 const app = express()
 var mysqlconnection = mysql.createConnection(
     {
-        host: 'localhost',
-        user: 'root',
-        password: 'amogh555kashyap',
-        database: 'hospital'
+        host: 'by59qgkgtip5p358uwli-mysql.services.clever-cloud.com',
+        user: 'ugpgztx7leeszzit',
+        password: 'JyYlZ2HheQD5e8zt82Ho',
+        database: 'by59qgkgtip5p358uwli'
     }
 );
 module.exports = mysqlconnection
+
+// mysql://ugpgztx7leeszzit:JyYlZ2HheQD5e8zt82Ho@by59qgkgtip5p358uwli-mysql.services.clever-cloud.com:3306/by59qgkgtip5p358uwli
